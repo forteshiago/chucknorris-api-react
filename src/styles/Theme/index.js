c02a27da717e853e058584428dbd7d165799b93a
forteshiago <forteshiago@hotmail.com>
2021-03-07 06:02:22 -0300

    Estilizacao global inicial; fontes; reset css;

---
+++ src/styles/Theme/index.js
@@ -0,0 +1,13 @@
+import { createMuiTheme } from '@material-ui/core/styles';
+import { ptBR } from '@material-ui/core/locale';
+
+export default createMuiTheme(
+  {
+    palette: {
+      secondary: {
+        main: '#48CCCC',
+      },
+    },
+  },
+  ptBR
+);
